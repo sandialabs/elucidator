7bc09b2df4dea347f795a0558cd25d2834a0112b
Joshua Brenneman Teves <jbteves@sandia.gov>
2024-06-27 17:26:25 -0400

    Start Python API

--- pyelucidator/src/lib.rs
+++ pyelucidator/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use elucidator::MemberSpecification;
+use elucidator::error::*;
+use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+
+#[pyclass(name = "MemberSpec")]
+struct MemberSpecPy {
+    inner: MemberSpecification,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[pymethods]
+impl MemberSpecPy {
+    #[staticmethod]
+    fn from_string(s: &str) -> PyResult<Self> {
+        match MemberSpecification::from(s) {
+            Ok(o) => Ok(Self { inner: o }),
+            Err(e) => {
+                Err(PyValueError::new_err("encountered a parsing error"))
+            }
+        }
     }
+}
+
+
+/// Elucidating my metadata
+#[pymodule]
+fn pyelucidator(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<MemberSpecPy>()?;
+    Ok(())
 }
