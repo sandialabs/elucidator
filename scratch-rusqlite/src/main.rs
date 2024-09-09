15300dd91962e3f27a2d1fe08b7fe78c862313db
Joshua Brenneman Teves <jbteves@sandia.gov>
2024-09-09 17:58:34 -0400

    WIP

--- scratch-rusqlite/src/main.rs
+++ scratch-rusqlite/src/main.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+use elucidator::{value::DataValue, designation::DesignationSpecification};
 use rusqlite::{Connection, Result};
+
+type Datum<'a> = HashMap<&'a str, DataValue>;
 
 #[derive(Debug)]
 struct DesignationSpec {
@@ -8,62 +12,163 @@ struct DesignationSpec {
 
 #[derive(Debug)]
 struct Metadata<'a> {
-    xmin: f64,
-    xmax: f64,
-    ymin: f64,
-    ymax: f64,
-    zmin: f64,
-    zmax: f64,
-    tmin: f64,
-    tmax: f64,
-    designation: &'a str,
-    buffer: &'a [u8],
+    pub xmin: f64,
+    pub xmax: f64,
+    pub ymin: f64,
+    pub ymax: f64,
+    pub zmin: f64,
+    pub zmax: f64,
+    pub tmin: f64,
+    pub tmax: f64,
+    pub designation: &'a str,
+    pub buffer: &'a [u8],
+}
+
+struct Database<'a> {
+    /// Active database connection
+    conn: Connection,
+    /// Mapping of designations
+    designations: HashMap<&'a str, DesignationSpecification>,
+}
+
+impl<'a, 'b> Database<'a> {
+    fn new(filename: Option<&str>) -> Result<Self> {
+        let db = if let Some(name) = filename {
+            Database {
+                conn: Connection::open(name)?,
+                designations: HashMap::new(),
+            }
+        } else {
+            Database {
+                conn: Connection::open_in_memory()?,
+                designations: HashMap::new(),
+            }
+        };
+        db.initialize()?;
+        Ok(db)
+    }
+    fn from(filename: &str) -> Result<Self> {
+        todo!("Implement pulling specs from db and creating designation map");
+        Ok(Database { conn: Connection::open(filename)?, designations: HashMap::new() })
+    }
+    fn initialize(&self) -> Result<()> {
+        self.conn.execute(
+            "CREATE TABLE designation_spec (
+                designation  TEXT NOT NULL PRIMARY KEY,
+                spec  TEXT NOT NULL
+            )",
+            (), // empty list of parameters.
+        )?;
+        self.conn.execute(
+            "CREATE TABLE Metadata (
+                xmin REAL,
+                xmax REAL,
+                ymin REAL,
+                ymax REAL,
+                zmin REAL,
+                zmax REAL,
+                tmin REAL,
+                tmax REAL,
+                designation  TEXT NOT NULL,
+                buffer  BLOB
+            )",
+            (), // empty list of parameters.
+        )?;
+        Ok(())
+    }
+    fn insert_spec_text(&mut self, designation: &'a str, spec: &'b str) -> Result<()> {
+
+        self.conn.execute(
+            "INSERT INTO designation_spec (designation, spec) VALUES (?1, ?2)",
+            (designation, spec),
+        )?;
+        let spec = DesignationSpecification::from_text(spec).unwrap();
+        self.designations.insert(designation, spec);
+        Ok(())
+    }
+    fn insert_metadata(&self, datum: &Metadata) -> Result<()> {
+        self.conn.execute(
+            "INSERT INTO Metadata (xmin, xmax, ymin, ymax, zmin, zmax, tmin, tmax, designation, buffer) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)",
+            (
+                &datum.xmin,
+                &datum.xmax,
+                &datum.ymin,
+                &datum.ymax,
+                &datum.zmin,
+                &datum.zmax,
+                &datum.tmin,
+                &datum.tmax, 
+                &datum.designation,
+                &datum.buffer, 
+            ),
+        )?;
+        Ok(())
+    }
+    fn get_metadata_in_bb(
+        &self,
+        xmin: f64, xmax: f64,
+        ymin: f64, ymax: f64,
+        zmin: f64, zmax: f64,
+        tmin: f64, tmax: f64,
+        designation: &str,
+        epsilon: Option<f64>,
+    ) -> Result<Vec<Datum>> {
+        let eps = epsilon.unwrap_or(0.0);
+        let xmin = xmin - eps;
+        let xmax = xmax + eps;
+        let ymin = ymin - eps;
+        let ymax = ymax + eps;
+        let zmin = zmin - eps;
+        let zmax = zmax + eps;
+        let tmin = tmin - eps;
+        let tmax = tmax + eps;
+        let mut stmt = self.conn.prepare_cached(
+            "SELECT buffer
+            FROM Metadata
+            WHERE
+                xmin >= ?1 AND
+                xmax <= ?2 AND
+                ymin >= ?3 AND
+                ymax <= ?4 AND
+                zmin >= ?5 AND
+                zmax <= ?6 AND
+                tmin >= ?7 AND
+                tmax <= ?8 AND
+                designation = ?9
+            ;
+            "
+        )?;
+
+        stmt.raw_bind_parameter(1, xmin)?;
+        stmt.raw_bind_parameter(2, xmax)?;
+        stmt.raw_bind_parameter(3, ymin)?;
+        stmt.raw_bind_parameter(4, ymax)?;
+        stmt.raw_bind_parameter(5, zmin)?;
+        stmt.raw_bind_parameter(6, zmax)?;
+        stmt.raw_bind_parameter(7, tmin)?;
+        stmt.raw_bind_parameter(8, tmax)?;
+        stmt.raw_bind_parameter(9, designation)?;
+
+        let mut rows = stmt.raw_query();
+        let mut data = Vec::new();
+        while let Some(row) = rows.next()? {
+            let buffer = match row.get_ref(0)? {
+                rusqlite::types::ValueRef::Blob(b) => b,
+                _ => unreachable!("We should always retrieve blobs!"),
+            };
+            let d = self.designations.get(designation).unwrap();
+            data.push(d.interpret_enum(&buffer).unwrap());
+        }
+        Ok(data)
+    }
 }
 
 fn main() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
-
-    conn.execute(
-        "CREATE TABLE designation_spec (
-            designation  TEXT NOT NULL PRIMARY KEY,
-            spec  TEXT NOT NULL
-        )",
-        (), // empty list of parameters.
-    )?;
-    conn.execute(
-        "CREATE TABLE Metadata (
-            xmin REAL,
-            xmax REAL,
-            ymin REAL,
-            ymax REAL,
-            zmin REAL,
-            zmax REAL,
-            tmin REAL,
-            tmax REAL,
-            designation  TEXT NOT NULL,
-            buffer  BLOB
-        )",
-        (), // empty list of parameters.
-    )?;
-     
-    let entry  = DesignationSpec {
-        designation: "FooBar".to_string(),
-        spec: "foo: u32, bar: i8".to_string(),
-    };
-    conn.execute(
-        "INSERT INTO designation_spec (designation, spec) VALUES (?1, ?2)",
-        (&entry.designation, &entry.spec),
-    )?;
-
-    let mut stmt = conn.prepare("SELECT designation, spec FROM designation_spec")?;
-    let designation_iter = stmt.query_map([], |row| {
-        Ok(DesignationSpec {
-            designation: row.get(0)?,
-            spec: row.get(1)?,
-        })
-    })?;
+    let mut db = Database::new(None)?;
     let designation = "FooBar";
-    let buffer = &[0; 9];
+    let spec = "foo: u32, bar: i8";
+    let buffer = &[7, 0, 0, 0, unsafe { std::mem::transmute::<i8, u8>(-5) }];
+    db.insert_spec_text(designation, spec)?;
     let md = Metadata {
         xmin: 0.0,
         xmax: 0.0,
@@ -76,46 +181,16 @@ fn main() -> Result<()> {
         designation,
         buffer,
     };
-    conn.execute(
-        "INSERT INTO Metadata (xmin, xmax, ymin, ymax, zmin, zmax, tmin, tmax, designation, buffer) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)",
-        (
-            &md.xmin,
-            &md.xmax,
-            &md.ymin,
-            &md.ymax,
-            &md.zmin,
-            &md.zmax,
-            &md.tmin,
-            &md.tmax, 
-            &md.designation,
-            &md.buffer, 
-        ),
-    )?;
+    db.insert_metadata(&md)?;
+    let data = db.get_metadata_in_bb(
+        -1.0, 1.0,
+        -1.0, 1.0,
+        -1.0, 1.0,
+        -1.0, 1.0,
+        designation,
+        None
+    ).unwrap();
+    println!("{data:#?}");
 
-
-    for designation in designation_iter {
-        println!("Found designation {:?}", designation.unwrap());
-    }
-    
-    println!("Here");
-    let mut stmt = conn.prepare("SELECT xmin, xmax, ymin, ymax, zmin, zmax, tmin, tmax, designation, buffer FROM Metadata")?;
-    println!("Now Here");
-    let mut rows = stmt.query([])?;
-    while let Some(row) = rows.next()? {
-        println!("And Now Here");
-        let x = Metadata {
-            xmin: row.get_unwrap(0),
-            xmax: row.get_unwrap(1),
-            ymin: row.get_unwrap(2),
-            ymax: row.get_unwrap(3),
-            zmin: row.get_unwrap(4),
-            zmax: row.get_unwrap(5),
-            tmin: row.get_unwrap(6),
-            tmax: row.get_unwrap(7), 
-            designation: row.get_ref_unwrap(8).as_str().unwrap(),
-            buffer: row.get_ref_unwrap(9).as_blob().unwrap(),
-        };
-        println!("{x:?}");
-    }
     Ok(())
 }
