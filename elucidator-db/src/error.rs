b03f215f69248b43827fe892b637f7006bc5e635
Curtis Brinker <cjbrin@sandia.gov>
2024-09-18 07:21:33 -0600

    Add rtree to database

--- elucidator-db/src/error.rs
+++ elucidator-db/src/error.rs
@@ -8,6 +8,12 @@ pub enum DatabaseError {
     },
     ElucidatorError{
         reason: elucidator::error::ElucidatorError,
+    },
+    VersionError{
+        reason: String
+    },
+    ConfigError{
+        reason: String
     }
 }
 
@@ -20,6 +26,12 @@ impl fmt::Display for DatabaseError {
             Self::ElucidatorError{reason} => {
                 format!("Elucidator Error: {reason}")
             },
+            Self::VersionError { reason } => {
+                format!("Version Error: {reason}")
+            },
+            Self::ConfigError { reason } => {
+                format!("Config Error: {reason}")
+            }
         };
         write!(f, "{m}")
     }
